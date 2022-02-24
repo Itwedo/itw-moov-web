692631082611aa3425a03911d10a7b4a69a95b96
Thomas Ayih-Akakpo <thomas.ayih-akakpo@acipenser.mg>
2022-02-24 14:46:44 +0300

    adding exchange rate route

--- app/__init__.py
+++ app/__init__.py
@@ -162,6 +162,28 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/taux-de-change.html")
+def exchange_rates():
+    today = date.today().strftime('%Y-%m-%d')
+    response = requests.get(
+        url=f"{STRAPI_API_URL}/exchangerates",
+        headers=STRAPI_API_AUTH_TOKEN,
+        params={
+            "filters[date][$eq]": today,
+            "filters[currency][$in]": ["USD", "EUR"]
+        }
+    )
+    result = [
+        {
+            'currency': i['attributes']['currency'],
+            'value': round(1/i['attributes']['rate'])
+        }
+        for i in response.json()['data']
+    ]
+    print(result)
+    return render_template('exchange.html', result=result, date=date.today().strftime('%d/%m/%Y'))
+    
+
 @app.route("/pharmacie.html")
 def pharmacie():
     today = date.today().strftime('%Y-%m-%d')
