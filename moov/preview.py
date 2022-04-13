c705c49ada605f284f6c504e874921d0cba0a371
HappyCat017 <mahefamampionona@gmail.com>
2022-04-13 17:44:05 +0300

    fix error in preview

--- moov/preview.py
+++ moov/preview.py
@@ -8,6 +8,7 @@ from flask import (
     request,
     render_template,
     send_from_directory,
+    url_for,
 )
 
 from .config import *
@@ -28,7 +29,7 @@ def preview_page(slug):
         params={
             "populate": "images",
             "publicationState": "preview",
-            "filters[slug][$eq]": slug,
+            "filters[slug][$eq]": f"https://moov.sudo.mg{url_for('preview.preview_page', slug=slug)}",
         },
         headers=STRAPI_API_AUTH_TOKEN,
     )
