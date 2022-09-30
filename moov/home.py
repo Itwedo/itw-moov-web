ce476eb1a6927ece329a4bf402d7d9eec43da57a
HappyCat017 <mahefamampionona@gmail.com>
2022-09-30 12:40:07 +0300

    articles publication dates

--- moov/home.py
+++ moov/home.py
@@ -100,7 +100,7 @@ def home():
         "flashes": [
             {
                 "head": item["attributes"]["head"],
-                "createdAt": item["attributes"]["createdAt"],
+                "createdAt": item["attributes"]["date"],
                 "id": item["id"],
                 "article": True if item["attributes"].__contains__('category') else False,
                 "slugId": item["attributes"]["slugId"] if item["attributes"].__contains__('slugId') else ""
