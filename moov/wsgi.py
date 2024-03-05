aaf0dd6728982fbebfaac08848d8fab49c5b4937
Cedric <andriamhajanirinacedric@gmail.com>
2024-03-05 16:20:01 +0300

    bloqué css

--- moov/wsgi.py
+++ moov/wsgi.py
@@ -1,5 +1,24 @@
 from moov.__init__ import app as application
+import glob
+import subprocess
+
+# Chemin vers le dossier des templates
+templates_path = 'moov/templates/*.html'  # Assurez-vous que le chemin correspond à l'emplacement de vos templates
+template_files = glob.glob(templates_path, recursive=True)  # Utilisez recursive=True si vous avez des sous-dossiers
+
+css_files = glob.glob('moov/assets/styles/*.css', recursive=True)
+
+def reload_install():
+    try:
+        # Exécute la commande dans le shell
+        subprocess.check_call(['pip', 'install', '.', '--no-deps' , '--ignore-installed'])
+        print("Le paquet a été installé avec succès sans dépendances.")
+    except subprocess.CalledProcessError as e:
+        print("Une erreur s'est produite lors de l'installation du paquet.")
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
+    reload_install()
+
+    print("############# " )
+    application.run(host="0.0.0.0", port=8080, debug=True, threaded=True , extra_files=template_files + css_files)
     
